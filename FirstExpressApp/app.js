22a31a150b01f469243342562d1b14192c937fa8
AMutaa <piusandru@gmail.com>
2018-06-13 17:03:03 -0400

    express listening

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -1,2 +1,16 @@
 var express = require("express");
 var app = express();
+
+// "/" => "Hi there!"
+app.get("/", function(req, res) {
+  res.send("Hi there!");
+});
+
+// "/bye" => "Goodbye!"
+
+//Tell Express to listen for requests(start server)
+
+// app.listen(3000, () => console.log("Server has started!!!"));
+app.listen(3000, function() {
+  console.log("Server has started!!");
+});
