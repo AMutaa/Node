aeac78cf12a1dc1c85550f96b57d76a42a6e3022
AMutaa <piusandru@gmail.com>
2018-06-14 15:07:39 -0400

    make life easy with a framework

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -1,37 +1,43 @@
 var express = require('express');
 var app = express();
 
+
+
+
+app.use(express.static('public')); /*serve other files in my app*/
+app.set('view engine', 'ejs'); /*automatimatically expect ejs files*/
+
 app.get('/', function (req, res) {
- res.render('home.ejs');
+  res.render('home');
 })
 
 app.get('/fallinlovewith/:thing', function (req, res) {
- var thing = req.params.thing;
- res.render('love.ejs', {
-  thingVar: thing
- });
+  var thing = req.params.thing;
+  res.render('love', {
+    thingVar: thing
+  });
 });
 
 app.get('/posts', function (req, res) {
- var posts = [{
-   title: 'Post 1',
-   author: 'Suzie'
-  },
+  var posts = [{
+      title: 'Post 1',
+      author: 'Suzie'
+    },
 
-  {
-   title: 'My adorable pet bunny',
-   author: 'Charlie'
-  },
+    {
+      title: 'My adorable pet bunny',
+      author: 'Charlie'
+    },
 
-  {
-   title: 'Can you believe this Pomsky',
-   author: 'Pius'
-  }
- ];
+    {
+      title: 'Can you believe this Pomsky',
+      author: 'Pius'
+    }
+  ];
 
- res.render('posts.ejs', {
-  posts: posts
- });
+  res.render('posts.ejs', {
+    posts: posts
+  });
 })
 
 
@@ -42,5 +48,5 @@ app.get('/posts', function (req, res) {
 
 
 app.listen(5000, function () {
- console.log('Server is listening!!!')
+  console.log('Server is listening!!!')
 })
