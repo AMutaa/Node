760dea42e09dff9b0fde8893429c7e42755e0c99
AMutaa <piusandru@gmail.com>
2018-06-14 11:38:35 -0400

    route params clear() exit quit exit

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -15,6 +15,21 @@ app.get("/bye", function(req, res) {
 app.get("/dog", function(req, res) {
   res.send("MEOW!");
 });
+
+// Route Parameters
+app.get("/r/:subredditName", function(req, res) {
+  var subreddit = req.params.subredditName;
+  res.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT!");
+});
+
+app.get("/r/:subredditName/comments/:id/:title/", function(req, res) {
+  res.send("WELCOME TO THE COMMENTS PAGE!");
+});
+
+//  "*" => "PAGE NOT FOUND"
+app.get("*", function(req, res) {
+  res.send("PAGE NOT FOUND!");
+});
 //Tell Express to listen for requests(start server)
 
 // app.listen(3000, () => console.log("Server has started!!!"));
