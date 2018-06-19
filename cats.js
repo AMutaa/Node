11a9742537cd8e9ee48424fcd6096b07c874a063
AMutaa <piusandru@gmail.com>
2018-06-19 13:33:44 -0400

    adding to a database

--- cats.js
+++ cats.js
@@ -5,7 +5,30 @@ mongoose.connect("mongodb://localhost/cat_app");
 var catSchema = new mongoose.Schema({
  name: String,
  age: Number,
- temparament: String
+ temperament: String
 });
 
-var Cat = mongoose.mode("Cat, catSchema");
+var Cat = mongoose.model("Cat", catSchema);
+
+// adding a new cat to the database
+
+var george = new Cat({
+ name: "Mrs. Norris",
+ age: 7,
+ temparament: "Evil"
+});
+
+// george.save();
+
+george.save(function (err, cat) {
+ if (err) {
+  console.log("something went wrong")
+ } else {
+  console.log("We just saved a cat to the database")
+  console.log(cat);
+ }
+});
+
+
+
+// retrieving cats from a database
