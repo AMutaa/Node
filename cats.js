a1ccc95f40a159e5347e544da0ef1f2748931b2b
AMutaa <piusandru@gmail.com>
2018-06-19 15:46:40 -0400

    mongoose2

--- cats.js
+++ cats.js
@@ -10,25 +10,47 @@ var catSchema = new mongoose.Schema({
 
 var Cat = mongoose.model("Cat", catSchema);
 
-// adding a new cat to the database
 
-var george = new Cat({
- name: "Mrs. Norris",
- age: 7,
- temparament: "Evil"
-});
-
-// george.save();
-
-george.save(function (err, cat) {
+Cat.create({
+ name: "Snow White",
+ age: 15,
+ temperament: "Bland"
+}, function (err, cat) {
  if (err) {
-  console.log("something went wrong")
+  console.log(err);
  } else {
-  console.log("We just saved a cat to the database")
   console.log(cat);
  }
-});
+})
+// adding a new cat to the database
+
+// var george = new Cat({
+//  name: "Mrs. Norris",
+//  age: 7,
+//  temparament: "Evil"
+// });
+
+// // george.save();
+
+// // george.save(function (err, cat) {
+// //  if (err) {
+// //   console.log("something went wrong")
+// //  } else {
+// //   console.log("We just saved a cat to the database")
+// //   console.log(cat);
+// //  }
+// // });
 
 
 
 // retrieving cats from a database
+
+Cat.find({}, function (err, cats) {
+ if (err) {
+  console.log("OH NO, ERROR!");
+  console.log(err);
+ } else {
+  console.log("ALL THE CATS....");
+  console.log(cats);
+ }
+})
